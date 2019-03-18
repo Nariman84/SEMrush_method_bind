89b15bfffb62e1e9f0a7a0b0792d38d24a001abd
nariman <bluesman.in@gmail.com>
2019-03-18 21:53:51 +0300

    SEMrush_bind_question-4

---
+++ js/script.js
@@ -0,0 +1,53 @@
+function bind(method, context) {
+	var args = Array.prototype.slice.call(arguments, 2);
+	return function() {
+		var a = args.concat(Array.prototype.slice.call(arguments, 0));
+		return method.apply(context, a);
+	}
+}
+
+var guitar = document.querySelector('.guitar');
+var priceGuitar = document.querySelector('.price-guitar');
+
+var product1 = {
+	name: 'Fender Stratocaster',
+	price: 1000
+}
+
+var product2 = {
+	name: 'Gibson SG',
+	price: 750
+}
+
+product1.sendMessageAboutBuying = function(buyer, cash) {
+	if (this.price > cash) {
+		alert(`${buyer}, у Вас не достаточно средств для покупки ${this.name}`);
+	} else {
+		alert(`${buyer}, поздравляем вас с приобретением ${this.name}`);
+	}
+}
+
+guitar.addEventListener('change', function() {
+	if (guitar.value == 'Fender Stratocaster') {
+		priceGuitar.innerText = '$' + product1.price;
+	} else {
+		priceGuitar.innerText = '$' + product2.price;
+	}
+});
+
+var btn = document.querySelector('.buy');
+btn.addEventListener('click', function() {
+	var product;
+	if (guitar.value == 'Fender Stratocaster') {
+		product = product1;
+	} else {
+		product = product2;
+	}
+
+	//привязка функции send к контексту
+	var sendMessage = bind(product1.sendMessageAboutBuying, product);
+
+	var buyerName = document.querySelector('.buyerName').value,
+		cash = document.querySelector('.cash').value;
+	sendMessage(buyerName, cash);
+});
